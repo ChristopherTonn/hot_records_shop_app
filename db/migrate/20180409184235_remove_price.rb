ec2350096c176af84e6739328429e931bbf7c60c
ChristopherTonn <christopher.tonn@gmail.com>
2018-04-09 20:44:08 +0200

    init

---
+++ db/migrate/20180409184235_remove_price.rb
@@ -0,0 +1,5 @@
+class RemovePrice < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :products, :price
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409181311) do
+ActiveRecord::Schema.define(version: 20180409184235) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20180409181311) do
     t.string "colour"
     t.string "music_label"
     t.string "release_date"
-    t.decimal "price"
   end
 
 end
