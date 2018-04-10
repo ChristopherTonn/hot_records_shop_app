dfaa7810108bd9ddff822f1ed2a99c3be2ad723d
ChristopherTonn <christopher.tonn@gmail.com>
2018-04-10 17:06:49 +0200

    4.2 	•Bugfix

--- db/migrate/20180410142407_add_new_values.rb
+++ db/migrate/20180410142407_add_new_values.rb
@@ -2,6 +2,6 @@ class AddNewValues < ActiveRecord::Migration[5.1]
   def change
     add_column :products, :price, :decimal
     add_column :products, :music_label, :string
-    add_column products, :release_date, :string
+    add_column :products, :release_date, :string
   end
 end
