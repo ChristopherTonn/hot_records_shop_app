f4c45c434097af29701d6d9ddb88be57dc109387
ChristopherTonn <christopher.tonn@gmail.com>
2018-04-10 13:56:01 +0200

    4.2 	•Bugfix again

--- db/migrate/20180406100457_create_products.rb
+++ db/migrate/20180406100457_create_products.rb
@@ -4,6 +4,9 @@ class CreateProducts < ActiveRecord::Migration[5.1]
       t.string :name
       t.text :description
       t.string :image_url
+      t.decimal :product_price
+      t.string :music_label
+      t.string :release_date
 
       t.timestamps
     end
--- db/migrate/20180409100701_add_color_to_products.rb
+++
@@ -1,7 +0,0 @@
-class AddColorToProducts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :products, :price, :decimal
-    add_column :products, :music_label, :string
-    add_column :products, :release_date, :string
-  end
-end
