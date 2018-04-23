ad0f03fec57b18fc7358bd3ea7c95f53ff9912db
ChristopherTonn <christopher.tonn@gmail.com>
2018-04-23 14:01:37 +0200

    5.3 	• Add input valid to contact form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,4 @@
 class Product < ApplicationRecord
-
-  product = Product.create(
-    name: "Test Product",
-    description: "Lorem Ipsum",
-    image_url: "https://oye-records.com/media/__sized__/releases/petar-dundov-marc-romboy-caper-tran-ep-crop-c0-5__0-5-600x600-70.jpg"
-  )
-
   has_many :orders
 
 
