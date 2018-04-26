4e2c6738fe5f4aebac0a74ea84e5b5148a0650d1
ChristopherTonn <christopher.tonn@gmail.com>
2018-04-26 18:24:56 +0200

    5.7 • Add Comment Model  • Implement the Comment in Product & User  • Add basic style to Product show

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,22 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
+
 
 
   def self.search(search_term)
+    if Rails.env.production?
+      Product.where("name ilike ?", "%#{search_term}%")
+    else
+      Product.where("name LIKE ?", "%#{search_term}%")
+    end
+  end
 
-    # For the local
-    # Product.where("name LIKE ?", "%#{search_term}%")
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
 
-    # For the production
-    Product.where("name ilike ?", "%#{search_term}%")
+  def lowest_rating_comment
+    comments.rating_asc.first
   end
 end
