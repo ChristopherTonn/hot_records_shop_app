29063a44011a20a74c30705a96ba65fd30645e1f
ChristopherTonn <christopher.tonn@gmail.com>
2018-05-15 16:10:00 +0200

    6.2 • Add new Unit Test for the Product Class

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
 describe Product do
-  #
-  # before do
-  #   product.comments.create!(rating: 1, user: user, body: "Awful bike!")
-  #   product.comments.create!(rating: 3, user: user, body: "Ok bike!")
-  #   product.comments.create!(rating: 5, user: user, body: "Great bike!")
-  # end
-  #
-  # it "returns the average rating of all comments" do
-  #
-  # end
-  #
-  #   context "when the product has comments" do
-  #     let(:product){ Product.create!(name: "Trace EP") }
-  #   end
+
+  before do
+    @product = Product.create!(description: "Nice Bla bla Sound. Mega bla bla Destrotion. Superb",name: "HOT REC. EP")
+    @user = User.create!(first_name: "Max",email: "ma@gmail.com", last_name: "Muster",password: "123123")
+    @product.comments.create!(rating: 1, user: @user, body: "Awful Destrotion!")
+    @product.comments.create!(rating: 3, user: @user, body: "Average Sound!")
+    @product.comments.create!(rating: 5, user: @user, body: "Beautiful EP! Love it!")
+  end
+
+  it "returns the average rating of all comments" do
+    expect(@product.average_rating).to eq(3)
+  end
+
+  it "is not valid" do
+    expect(Product.new(description: "Nice flower")).not_to be_valid
+  end
+
 end
