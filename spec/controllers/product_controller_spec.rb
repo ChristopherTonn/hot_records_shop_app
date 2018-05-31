16cadf3b8552a8876fa773a8fc526e9be1cabc2e
ChristopherTonn <christopher.tonn@gmail.com>
2018-05-31 20:00:56 +0200

    6.15 • Add more Test to: - product and product_controller, • Create comments_spec File and add test's

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -7,6 +7,7 @@ describe ProductsController, focus: true, type: :controller do
     @product = FactoryBot.create(:product, name: "Test Product", description: "Test product", price: 6.33)
   end
 
+  #---GET----
   context 'GET #index' do
     it 'renders the index template' do
       get :index
@@ -22,6 +23,51 @@ describe ProductsController, focus: true, type: :controller do
       expect(response).to render_template('new')
     end
   end
+
+  context 'GET #show' do
+    it 'renders show template' do
+      get :show, params: { id: @product.id }
+      expect(response).to be_ok
+      expect(response).to render_template('show')
+  end
+
+  context 'GET #edit' do
+    it "renders edit template" do
+      get :edit, params: { id: @product.id }
+      expect(response).to be_ok
+      expect(response).to render_template('edit')
+    end
+  end
+
+
+  #---POST----
+  context "POST #create" do
+    it "if attr are correct shows product page" do
+      @product = Product.create!(name: "Hot Records", description: "Nice Bla bla.", image_url: "hotrecods.jpg", price: "10")
+      post :create, params: { product: { name: @product.name, description: @product.description, image_url: @product.image_url, price: @product.price} }
+      expect(response).to redirect_to(:action => :show, :id => assigns(:product).id)
+    end
+  end
+
+  context "POST #update" do
+    it "updates product attributes" do
+      @update = { name: "Not A Hot Record"}
+      post :update, params: {id: @product.id, product: @update }
+      @product.reload
+      expect(@product.name).to eq "Not A Hot Record"
+    end
+  end
+
+  #---POST----
+  context "DELETE #destroy" do
+    it "destroy product and go back to products_path" do
+      delete :destroy, params: { id: @product.id}
+      expect(response).to redirect_to products_path
+    end
+  end
+  end
+
+
 
 
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Product do
 
   before do
-    @product = Product.create!(description: "Nice Bla bla Sound. Mega bla bla Destrotion. Superb",name: "HOT REC. EP")
+    @product = Product.create!(name: "Hot Records", description: "Nice Bla bla.", image_url: "hotrecods.jpg", price: "10")
     @user = User.create!(first_name: "Max",email: "ma@gmail.com", last_name: "Muster",password: "123123")
     @product.comments.create!(rating: 1, user: @user, body: "Awful Destrotion!")
     @product.comments.create!(rating: 3, user: @user, body: "Average Sound!")
@@ -17,5 +17,25 @@ describe Product do
   it "is not valid" do
     expect(Product.new(description: "Nice flow.")).not_to be_valid
   end
+
+
+  #Testing the rating of the comments
+  it "returns the average rating of all comments" do
+      expect(@product.average_rating).to eq 3
+  end
+
+  context "check the highest and lowest rating comment" do
+
+    it "returns the higest rating comment" do
+      expect(@product.highest_rating_comment.rating).to eq 5
+    end
+
+    it "returns the lowest rating comment" do
+      expect(@product.lowest_rating_comment.rating).to eq 1
+    end
+  end
+
+
+
 
 end
